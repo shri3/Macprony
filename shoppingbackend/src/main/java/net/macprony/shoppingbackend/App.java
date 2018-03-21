790e54da01fd41842d96f658de0f35a206250ad8
shri <shril113@gmail.com>
2018-03-21 21:47:23 +0530

    setup completed

---
+++ shoppingbackend/src/main/java/net/macprony/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.macprony.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
